5c616667f20276b0c11df8e3c61d61997ef8df4e
zhanghb22 <329458839@qq.com>
2016-08-23 17:36:55 +0800

    111

---
+++ src/main/java/com/love/origami/love_origami/app/App.java
@@ -0,0 +1,13 @@
+package com.love.origami.love_origami.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
